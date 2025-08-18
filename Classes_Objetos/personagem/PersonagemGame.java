5373449a41896d0e8dcce81361b5f3d58ace95b6
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-08-18 18:01:45 -0300

    Updates

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,20 +1,4 @@
 public class PersonagemGame {
     public int saudeAtual;
     public String nome;
-
-    public int getSaudeAtual() {
-        return saudeAtual;
-    }
-
-    public void setSaudeAtual(int saudeAtual) {
-        this.saudeAtual = saudeAtual;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
