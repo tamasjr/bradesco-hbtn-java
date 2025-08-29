b9105336ae056ad0b689c0bd9288832fb1ea892f
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-08-29 02:19:40 -0300

    modulo Array

---
+++ estruturas_de_dados/0/ArrayExercicio.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+public class ArrayExercicio {
+
+
+    public static void main(String[] args) {
+        // Criando o scanner para entrada de dados
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Definindo o tamanho do array
+        int tamanhoArray = 10;
+        int[] numeros = new int[tamanhoArray];
+        int soma = 0;
+        int maiorNumero = Integer.MIN_VALUE;
+
+
+        // Preenchendo o array com números fornecidos pelo usuário
+        // Utilize scanner.nextInt(); para receber o numero digitado
+        System.out.println("Digite 10 números inteiros:");
+
+
+        for (int i = 0; i < tamanhoArray; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+            soma += numeros[i];
+            if (numeros[i] > maiorNumero) {
+                maiorNumero = numeros[i];
+            }
+        }
+
+        System.out.println("Conteúdo do array: ");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.println("Soma de todos os números: " + soma);
+        System.out.println("Maior número no array: " + maiorNumero);
+
+        // Fechando o scanner
+        scanner.close();
+    }
+}
