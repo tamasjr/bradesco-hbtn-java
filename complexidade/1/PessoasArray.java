f403a1cd6dd144d36185f18077ab046beed9ba4a
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-09-01 13:44:48 -0300

    Complexidade O(n) – Busca Linear

---
+++ complexidade/1/PessoasArray.java
@@ -0,0 +1,37 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes = new String[]{"Amanda", "Beatriz", "Carlos", "Daniela", "Eduardo",
+                "Fabio", "Gustavo", "Hingrid", "Isabela", "Joao", "Leise", "Maria",
+                "Norberto", "Otavio", "Paulo", "Quirino", "Renata", "Sabata",
+                "Tais", "Umberto", "Vanessa", "Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    public void buscaLinear(String nome) {
+        System.out.println("Procurando pelo nome: \"" + nome + "\"");
+        boolean found = false;
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println("Passando pelo indice:" + i);
+            if (nomes[i].equalsIgnoreCase(nome)) {
+                System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
+                found = true;
+            }
+        }
+        if (!found) {
+            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
+    }
+
+}
+
+
