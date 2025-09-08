2c99336ab9fbaba2e2a3d6239d1006c5b633bafc
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-09-08 10:00:26 -0300

    Salario Funcionario

---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,15 @@
+public class Funcionario {
+    private double salario;
+
+    public Funcionario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
