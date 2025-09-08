5d408a84701a2d9f09538d2ab91a25e4bcee3906
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-09-08 17:12:22 -0300

    Relatório Produto com maior preço

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst()
+                .orElse(null);
+    }
+
+}
---
+++ Streams_Collectors_Optional/maior_preco/Pedido.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Pedido {
+    private int codigo;
+    private List<Produto> produtos;
+    private Cliente cliente;
+
+    public Pedido(int codigo, List<Produto> produtos, Cliente cliente) {
+        this.codigo = codigo;
+        this.produtos = produtos;
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+}
