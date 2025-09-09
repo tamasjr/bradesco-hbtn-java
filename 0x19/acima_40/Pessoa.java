13b49edc41e52020a3a5a86716e12f792864947a
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-09-09 15:43:51 -0300

    Pessoas agrupadas por cargo e que possuam pelo menos 40 anos

---
+++ 0x19/acima_40/Pessoa.java
@@ -0,0 +1,67 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.getDefault());
+        symbols.setDecimalSeparator(',');
+        DecimalFormat df = new DecimalFormat("0.000000", symbols);
+        return "["+codigo+"] "+nome+" "+cargo+" "+idade+ " R$ "+df.format(salario);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
