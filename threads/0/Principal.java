dd2dfaa7c3c2bbd0c9f4cfc3bb675212a675c9b5
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-09-26 11:25:51 -0300

    Update Principal.java

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -4,21 +4,17 @@ public class Principal {
 
     public static void main(String[] args) {
         Contador contador = new Contador();
-        List<Thread> threads = new ArrayList<>();
-        // Cria e inicia 10 threads
-        for (int i = 0; i < 2; i++) {
-            Thread thread = new ThreadContador(contador);
-            threads.add(thread);
-            thread.start();
-        }
-        // Aguarda todas as threads terminarem
-        for (Thread thread : threads) {
-            try {
-                thread.join();
+
+            Thread thread1 = new ThreadContador(contador);
+            Thread thread2 = new ThreadContador(contador);
+            thread1.start();
+            thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
 
         System.out.println("Valor final do contador: " + contador.getContagem());
     }
