dc4e9d2cdb715fd2f83b77b4ebf703c05e9fa29d
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-09-26 09:20:37 -0300

    Hobbies por cargo

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    private List<String> hobbies;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+        this.hobbies = hobbies;
+    }
+
+    public int getCodigo() { return codigo; }
+    public String getNome() { return nome; }
+    public String getCargo() { return cargo; }
+    public int getIdade() { return idade; }
+    public double getSalario() { return salario; }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %s", codigo, nome, cargo, idade, String.format("%.6f", salario).replace('.', ','));
+    }
+
+    @Override
+    public int compareTo(Pessoa other) {
+        return this.nome.compareToIgnoreCase(other.nome);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
