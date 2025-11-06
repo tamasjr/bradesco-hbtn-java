bff180578966ce03433dc4fae26f8eae26740de5
TAMAS PETER BATTONYAI JUNIOR <tamas.battonyai@bradesco.com.br>
2025-11-06 13:31:54 -0300

    Update PedidoService.java

--- ddd/0/application/PedidoService.java
+++ ddd/0/application/PedidoService.java
@@ -1,39 +1,45 @@
 package application;
 
-
 import domain.Cliente;
 import domain.Pedido;
 import domain.Produto;
 import infrastructure.PedidoRepository;
 
-
 public class PedidoService {
 
-
     private final PedidoRepository pedidoRepository;
-
 
     public PedidoService(PedidoRepository pedidoRepository) {
         this.pedidoRepository = pedidoRepository;
     }
 
-
     public Pedido criarPedido(Cliente cliente) {
-        // implementar
+        Pedido pedido = new Pedido(cliente);
+        pedidoRepository.salvar(pedido);
+        return pedido;
     }
-
 
     public void adicionarProduto(String pedidoId, Produto produto) {
-        // implementar
+        Pedido pedido = pedidoRepository.buscarPorId(pedidoId);
+        if (pedido != null) {
+            pedido.adicionarProduto(produto);
+            pedidoRepository.salvar(pedido);
+        }
     }
-
 
     public void pagarPedido(String pedidoId) {
-        // implementar
+        Pedido pedido = pedidoRepository.buscarPorId(pedidoId);
+        if (pedido != null) {
+            pedido.pagar();
+            pedidoRepository.salvar(pedido);
+        }
     }
 
-
     public void cancelarPedido(String pedidoId) {
-        // implementar
+        Pedido pedido = pedidoRepository.buscarPorId(pedidoId);
+        if (pedido != null) {
+            pedido.cancelar();
+            pedidoRepository.salvar(pedido);
+        }
     }
 }
